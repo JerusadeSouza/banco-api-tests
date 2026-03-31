3353a107957a73a7fbb7638a1c8435cab7066953
Jerusa de Souza <jerusa24souza@gmail.com>
2026-03-31 13:35:25 -0300

    adicionando a biblioteca .env para variaveis globais

---
+++ test/transferencia.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const { expect } = require('chai')
+require('dotenv').config()
+
+describe('Transferências', () => {
+    describe('POST /transferencia', () => {
+        it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de 10,00', async () => {
+            const respostaLogin = await request(process.env.BASE_URL)
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            
+            const token = respostaLogin.body.token
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 11,
+                    token: ""
+                })
+                expect(resposta.status).to.equal(201);
+        })
+
+        it('Deve retornar falha com 422 quando o valor da transferência for abaixo de 10,00', async () => {
+            const respostaLogin = await request(process.env.BASE_URL)
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            
+            const token = respostaLogin.body.token
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 7,
+                    token: ""
+                })
+                expect(resposta.status).to.equal(422);
+        })
+    })
+})
